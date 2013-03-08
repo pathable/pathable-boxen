d50543a68ce21144a247c7993d196636887f657d
Michael Phan-Ba <michael@mikepb.com>
2013-03-08 14:58:18 -0800

    Fix config

--- modules/pathable/manifests/environment.pp
+++ modules/pathable/manifests/environment.pp
@@ -2,6 +2,8 @@ class pathable::environment {
   include elasticsearch
   #include java
   include memcached
+  include mysql
+  include postgresql
   include redis
 
   mysql::db { 'pathable2_development': }
