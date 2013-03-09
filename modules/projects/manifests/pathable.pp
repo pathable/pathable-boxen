0708b7aafa478adadc7624ad08607ca5023ef7d9
Michael Phan-Ba <michael@mikepb.com>
2013-03-08 17:28:34 -0800

    Custom configure nginx

--- modules/projects/manifests/pathable.pp
+++ modules/projects/manifests/pathable.pp
@@ -7,7 +7,7 @@ class projects::pathable {
     elasticsearch => true,
     memcached     => true,
     mysql         => true,
-    nginx         => true,
+    nginx         => 'projects/shared/nginx-pathable.conf.erb',
     redis         => true,
     ruby          => '1.9.3',
     source        => 'pathable/pathable'
